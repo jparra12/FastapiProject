804fe1c568669ad065dfe1c69655eb18e98970ba
jparra12 <parrajesusalejandro@gmail.com>
2025-01-26 13:10:31 -0400

    Add pydantic validation to endpoint input

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi.params import Body
+from pydantic import BaseModel
+
+class Post (baseModel):
+    title:str
+    content:str
+    published= False
+    rating= Optional[int]=None
+
+
+
 
 app = FastAPI()
 
@@ -8,6 +18,7 @@ async def root():
     return {"message":"Hello World"}
 
 @app.post("/posts")
-async def create_posts(payLoad: dict = Body(...)):
-    print(payLoad)
-    return {"message": f"Title: {payLoad['Title']}, Content: {payLoad['Content']}"}
+async def create_posts(post: Post):
+    print(post)
+    print(post.dict())
+    return {"data": post}
